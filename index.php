d6166a1adf594750b0700ccd2f1e82e6ba404358
therealmensumit <sk03950@gmail.com>
2024-07-07 10:54:22 +0530

    "Updated CSS styles, removed hero section background image, and refactored HTML structure in index.php"

--- index.php
+++ index.php
@@ -1,42 +1,29 @@
 <?php include_once('include/header.php'); ?>
 
-<section class="hero">
+<!-- hero section -->
+<img src="./assets/img/hero-banner.jpg" alt="Hero Banner" class="img-fluid" />
+
+<section>
   <div class="container">
-    <div class="row">
-      <div class="col-12 col-lg-10 offset-lg-1 text-center">
-        <h1 class="hero-title">Lorem ipsum dolor sit</h1>
-        <p class="hero-subtitle mb-5">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet architecto asperiores minus.</p>
-        <form>
-          <div class="row g-3 g-lg-0">
-            <div class="col-12 col-sm-6 col-lg-3">
-              <input type="text" class="form-control" placeholder="Name" />
-            </div>
-            <div class="col-12 col-sm-6 col-lg-3">
-              <select name="" class="form-select">
-                <option value="Select Catagory">Select Catagory</option>
-                <option value="Escort">Escort</option>
-                <option value="Massage">Massage</option>
-              </select>
-            </div>
-            <div class="col-12 col-sm-6 col-lg-3">
-              <select name="" class="form-select">
-                <option value="Select State">Select State</option>
-                <option value="New Delhi">New Delhi</option>
-                <option value="Uttar Pradesh">Uttar Pradesh</option>
-                <option value="Haryana">Haryana</option>
-                <option value="Punjab">Punjab</option>
-              </select>
-            </div>
-            <div class="col-12 col-sm-6 col-lg-3 text-start">
-              <button type="submit" class="btn btn-primary px-4 w-100">Search</button>
+    <div class="row gy-5">
+      <div class="col-12 text-center">
+        <h1 class="text-primary">Call Girls in Lucknow Escorts Service</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eos ipsam itaque quia sit impedit enim nemo alias consectetur corporis tempore dolor iure fugiat, sed id minima doloremque quasi suscipit, beatae debitis earum, magnam blanditiis! Tempora unde consequatur ipsa assumenda deleniti. Dolores perspiciatis aliquid quibusdam doloribus dolor sequi quam, autem temporibus assumenda veniam reiciendis voluptates eius sint quod suscipit repellat! Nemo ipsum, perferendis facilis earum veritatis repellat soluta delectus. Quis numquam voluptatem porro dolores architecto velit maxime illo eveniet distinctio officia?</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis, deleniti alias cum tenetur aut velit deserunt consequatur harum aspernatur eaque magnam dolore culpa architecto necessitatibus labore numquam dignissimos quas soluta.</p>
+      </div>
+      <?php for ($i = 0; $i < 4; $i++) { ?>
+        <div class="col-lg-3">
+          <div class="card model-card">
+            <img src="./assets/img/escort-girl.jpg" alt="" class="card-img-top" />
+            <div class="card-body">
+              <h5 class="card-title">Model Name</h5>
+              <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis necessitatibus cum similique incidunt itaque eveniet nobis voluptatum animi laboriosam ipsum?</p>
             </div>
           </div>
-        </form>
-      </div>
+        </div>
+      <?php } ?>
     </div>
   </div>
 </section>
-
-<section></section>
 
 <?php include_once('include/footer.php'); ?>
