9f37652e0d0b82177d1c941e65838b93ef58e3b5
akkushwaha7834 <akkushwaha018@gmail.com>
2024-07-16 20:33:54 +0530

    sticky header removed

--- assets/js/script.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("scroll", function () {
-  const header = document.getElementById("header");
-  if (window.scrollY > 50) {
-    header.classList.add("bgc-primary", "position-fixed");
-  } else {
-    header.classList.remove("bgc-primary", "position-fixed");
-  }
-});
