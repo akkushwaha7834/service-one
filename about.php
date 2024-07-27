00b06327ee0257495c7b429f8b34e647d1c50e2b
amitfxcareers <amit@fxcareers.com>
2024-07-27 16:21:47 +0530

    removed extra code

--- about.php
+++ about.php
@@ -3,23 +3,7 @@
 <!-- hero section -->
 <img src="./assets/img/banner.webp" alt="Hero Banner" class="img-fluid pe-none banner-img" width="100%" />
 
-<section>
-    <div class="container">
-        <div class="row gy-4 align-items-center">
-            <div class="col-md-6">
-                <h2 class="text-primary">About Lucknow Escorts Service</h2>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro temporibus consectetur beatae aliquid iusto eius culpa laboriosam atque nisi deleniti obcaecati rem vero modi est recusandae officiis, dolor accusamus, placeat labore distinctio! Odio perferendis illo porro officia natus necessitatibus nostrum!</p>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro temporibus consectetur beatae aliquid iusto eius culpa laboriosam atque nisi deleniti obcaecati rem vero modi est recusandae officiis, dolor accusamus, placeat labore distinctio! Odio perferendis illo porro officia natus necessitatibus nostrum!</p>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro temporibus consectetur beatae aliquid iusto eius culpa laboriosam atque nisi deleniti obcaecati rem vero modi est recusandae officiis, dolor accusamus, placeat labore distinctio! Odio perferendis illo porro officia natus necessitatibus nostrum!</p>
-            </div>
-            <div class="col-md-6 col-lg-5 offset-lg-1">
-                <img src="./assets/img/top-rt-img.webp" alt="" class="img-fluid">
-            </div>
-        </div>
-    </div>
-</section>
-
-<section class="about-para bg-body-secondary">
+<section class="about-para ">
     <div class="container">
         <div class="row">
             <div class="col-md-12">
@@ -35,20 +19,5 @@
     </div>
 </section>
 
-<section>
-    <div class="container">
-        <div class="row gy-4 align-items-center">
-            <div class="col-md-6 col-lg-5 offset-lg-1">
-                <img src="./assets/img/top-rt-img.webp" alt="" class="img-fluid">
-            </div>
-            <div class="col-md-6">
-                <h2 class="text-primary">About Lucknow Escorts Service</h2>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro temporibus consectetur beatae aliquid iusto eius culpa laboriosam atque nisi deleniti obcaecati rem vero modi est recusandae officiis, dolor accusamus, placeat labore distinctio! Odio perferendis illo porro officia natus necessitatibus nostrum!</p>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro temporibus consectetur beatae aliquid iusto eius culpa laboriosam atque nisi deleniti obcaecati rem vero modi est recusandae officiis, dolor accusamus, placeat labore distinctio! Odio perferendis illo porro officia natus necessitatibus nostrum!</p>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro temporibus consectetur beatae aliquid iusto eius culpa laboriosam atque nisi deleniti obcaecati rem vero modi est recusandae officiis, dolor accusamus, placeat labore distinctio! Odio perferendis illo porro officia natus necessitatibus nostrum!</p>
-            </div>
-        </div>
-    </div>
-</section>
 
 <?php include_once('./include/footer.php') ?>
--- service.php
+++ service.php
@@ -19,23 +19,7 @@
     </div>
 </section>
 
-<section class="about-para bg-body-secondary">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <h4>About Lucknow Escorts Service</h4>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro temporibus consectetur beatae aliquid iusto eius culpa laboriosam atque nisi deleniti obcaecati rem vero modi est recusandae officiis, dolor accusamus, placeat labore distinctio! Odio perferendis illo porro officia natus necessitatibus nostrum! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro temporibus consectetur beatae aliquid iusto eius culpa laboriosam atque nisi deleniti obcaecati rem vero modi est recusandae officiis, dolor accusamus, placeat labore distinctio! Odio perferendis illo porro officia natus necessitatibus nostrum!
-                </p>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro temporibus consectetur beatae aliquid iusto eius culpa laboriosam atque nisi deleniti obcaecati rem vero modi est recusandae officiis, dolor accusamus, placeat labore distinctio! Odio perferendis illo porro officia natus necessitatibus nostrum! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro temporibus consectetur beatae aliquid iusto eius culpa laboriosam atque nisi deleniti obcaecati rem vero modi est recusandae officiis, dolor accusamus, placeat labore distinctio! Odio perferendis illo porro officia natus necessitatibus nostrum!
-                </p>
-            </div>
-        </div>
-    </div>
-</section>
-
-<section>
+<section class="bg-body-secondary">
     <div class="container">
         <div class="row gy-4 align-items-center">
             <div class="col-md-6 col-lg-5 offset-lg-1">
@@ -50,21 +34,22 @@
         </div>
     </div>
 </section>
-
-<section class="about-para bg-body-secondary">
+<section>
     <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <h4>About Lucknow Escorts Service</h4>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro temporibus consectetur beatae aliquid iusto eius culpa laboriosam atque nisi deleniti obcaecati rem vero modi est recusandae officiis, dolor accusamus, placeat labore distinctio! Odio perferendis illo porro officia natus necessitatibus nostrum! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro temporibus consectetur beatae aliquid iusto eius culpa laboriosam atque nisi deleniti obcaecati rem vero modi est recusandae officiis, dolor accusamus, placeat labore distinctio! Odio perferendis illo porro officia natus necessitatibus nostrum!
-                </p>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro temporibus consectetur beatae aliquid iusto eius culpa laboriosam atque nisi deleniti obcaecati rem vero modi est recusandae officiis, dolor accusamus, placeat labore distinctio! Odio perferendis illo porro officia natus necessitatibus nostrum! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro temporibus consectetur beatae aliquid iusto eius culpa laboriosam atque nisi deleniti obcaecati rem vero modi est recusandae officiis, dolor accusamus, placeat labore distinctio! Odio perferendis illo porro officia natus necessitatibus nostrum!
-                </p>
+        <div class="row gy-4 align-items-center">
+            <div class="col-md-6">
+                <h2 class="text-primary">About Lucknow Escorts Service</h2>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro temporibus consectetur beatae aliquid iusto eius culpa laboriosam atque nisi deleniti obcaecati rem vero modi est recusandae officiis, dolor accusamus, placeat labore distinctio! Odio perferendis illo porro officia natus necessitatibus nostrum!</p>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro temporibus consectetur beatae aliquid iusto eius culpa laboriosam atque nisi deleniti obcaecati rem vero modi est recusandae officiis, dolor accusamus, placeat labore distinctio! Odio perferendis illo porro officia natus necessitatibus nostrum!</p>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro temporibus consectetur beatae aliquid iusto eius culpa laboriosam atque nisi deleniti obcaecati rem vero modi est recusandae officiis, dolor accusamus, placeat labore distinctio! Odio perferendis illo porro officia natus necessitatibus nostrum!</p>
+            </div>
+            <div class="col-md-6 col-lg-5 offset-lg-1">
+                <img src="./assets/img/top-rt-img.webp" alt="" class="img-fluid">
             </div>
         </div>
     </div>
 </section>
+
+
 
 <?php include_once('include/footer.php'); ?>
